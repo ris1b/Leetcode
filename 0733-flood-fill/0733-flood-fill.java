357b26381f4a8055ab96fbd37852672714556d63
ris1b <101881146+ris1b@users.noreply.github.com>
2023-02-26 21:56:05 +0530

    Time: 1 ms (52.28%), Space: 43.9 MB (17.32%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,36 +1,21 @@
-class Pair{
-    int i; int j;
-    Pair (int i, int j){
-        this.i = i;
-        this.j = j;
-    }
-}
 class Solution {
-    static final int x[] = {-1, 1, 0, 0};
-    static final int y[] = {0,0,-1,1};
+    int x[] = {-1, 1, 0, 0};
+    int y[] = {0,0,-1,1};
 
     public int[][] floodFill(int[][] mat, int i, int j, int color) {
         if(mat[i][j] == color) return mat;
         
         int old = mat[i][j];        // say, 1
+        
         mat[i][j] = color;          // new, 2
 
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(i, j));
+        for(int id=0;id < x.length;id++){
+            int i1 = i + x[id];
+            int i2 = j + y[id];
+            if(check(i1, i2, mat, old)) 
+                floodFill(mat, i1, i2, color);
+        }
 
-        while(!q.isEmpty()){
-            Pair p = q.remove();
-            mat[p.i][p.j] = color;
-
-            // int old = mat[i][j];
-            for(int id=0; id<4; id++){
-                int r = p.i + x[id];
-                int c = p.j + y[id];
-                if(check(r,c,mat,old)){
-                    q.add(new Pair(r,c));
-                }
-            }
-        } 
         return mat;
     }
 
@@ -40,4 +25,6 @@ class Solution {
         }
         return true;
     }
+
+
 }
