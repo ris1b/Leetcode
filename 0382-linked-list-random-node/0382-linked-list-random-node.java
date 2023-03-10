cd69445b2bfb93172591627dbc8ae1b15ffd5438
ris1b <101881146+ris1b@users.noreply.github.com>
2023-03-10 22:48:54 +0530

    Time: 13 ms (35.62%), Space: 44.8 MB (17.81%) - LeetHub

--- 0382-linked-list-random-node/0382-linked-list-random-node.java
+++ 0382-linked-list-random-node/0382-linked-list-random-node.java
@@ -8,20 +8,9 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Pair{
-    int val;
-    int index;
-
-    Pair(int val, int index){
-        this.val = val;
-        this.index = index;
-    }
-}
 class Solution {
     HashMap<Integer, Integer> map;
     ListNode head;
-    int min = Integer.MAX_VALUE;
-    int max = Integer.MIN_VALUE;
     int n;
     public Solution(ListNode head) {
         this.head = head;
@@ -30,17 +19,12 @@ class Solution {
         
         while(temp != null){
             map.put(n, temp.val);
-            min = Math.min(min, temp.val);
-            max = Math.max(max, temp.val);
             temp = temp.next;
             n++;
         }
     }
     
     public int getRandom() {
-        int lo = min;
-        int hi = max;
-
         double rand =  107 * Math.random();
         int idx = (int)(rand % n + n) % n;
         
